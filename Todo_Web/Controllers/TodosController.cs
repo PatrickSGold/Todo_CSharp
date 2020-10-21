5adcb0f1ead53401c161070700db0c66883232f2
Patrick <patrickgold89@outlook.com>
2020-10-21 22:51:31 +0200

    Todos Controller Added

---
+++ Todo_Web/Controllers/TodosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Todo.Data.Services;
+
+namespace Todo_Web.Controllers
+{
+    public class TodosController : Controller
+    {
+        ITodoData db;
+
+        public TodosController()
+        {
+            db = new InMemoryData();
+        }
+        // GET: Todos
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+    }
+}
